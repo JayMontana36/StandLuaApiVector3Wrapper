eea30ab552905ae55be3031a5101c6efbefa629f
Jasen Samuels <jasensamuels@gmail.com>
2022-03-31 16:08:24 -0400

    Update StandLuaApiVector3Wrapper.lua

--- StandLuaApiVector3Wrapper.lua
+++ StandLuaApiVector3Wrapper.lua
@@ -1,6 +1,8 @@
-local DummyFunction = function()end
-
+--[[ Main - Internal Logic Stuff ]]
 local v3 = v3
+local setmetatable = setmetatable
+
+local DummyFunction = function()end
 
 local V3WrapperKeyFunctionsA = setmetatable(
 	{
@@ -50,28 +52,218 @@ local V3WrapperMetaTable =
 
 local V3WrapperArchive = setmetatable({},{ __mode	= "kv" })
 
-local v3_new = v3.new
-local V3NewFunctions =
-{
-	table	=	function(V3Table)
-					V3Table.V3 = v3_new(V3Table)
-					V3Table.x, V3Table.y, V3Table.z = nil, nil, nil
-					return V3Table
-				end,
-	number	=	function(x, args)
-					return { V3 = v3_new(x, args[2], args[3]) }
-				end,
-}
-local V3New = function(...)
-	local args = {...}
-	local arg1 = args[1]
+
+
+--[[ v3 Functions Overrides ]]
+do
+	local function WrapNewInstance(Instance)
+		local WrappedV3 = setmetatable({ V3 = Instance }, V3WrapperMetaTable)
+		table.insert(V3WrapperArchive, WrappedV3)
+		return WrappedV3
+	end
+	local Functions =
+	{
+		new			=	function()
+							local v3_new = v3.new
+							local V3NewFunctions =
+							{
+								table	=	function(V3Table)
+												V3Table.V3 = v3_new(V3Table)
+												V3Table.x, V3Table.y, V3Table.z = nil, nil, nil
+												return V3Table
+											end,
+								number	=	function(x, args)
+												return { V3 = v3_new(x, args[2], args[3]) }
+											end,
+							}
+							return function(...)
+								local args = {...}
+								local arg1 = args[1]
+								
+								local WrappedV3 = setmetatable(V3NewFunctions[type(arg1)](arg1, args), V3WrapperMetaTable)
+								
+								table.insert(V3WrapperArchive, WrappedV3)
+								
+								return WrappedV3
+							end
+							
+						end,
+		get			=	function()
+							local v3_get = v3.get
+							return function(WrappedV3)
+								return v3_get(WrappedV3.V3)
+							end
+						end,
+		getX		=	function()
+							local v3_getX = v3.getX
+							return function(WrappedV3)
+								return v3_getX(WrappedV3.V3)
+							end
+						end,
+		getY		=	function()
+							local v3_getY = v3.getY
+							return function(WrappedV3)
+								return v3_getY(WrappedV3.V3)
+							end
+						end,
+		getZ		=	function()
+							local v3_getZ = v3.getZ
+							return function(WrappedV3)
+								return v3_getZ(WrappedV3.V3)
+							end
+						end,
+		getHeading	=	function()
+							local v3_getZ = v3.getZ
+							return function(WrappedV3)
+								return v3_getZ(WrappedV3.V3)
+							end
+						end,
+		set			=	function() -- Make similar to new so that tables can be supplied?
+							local v3_set = v3.set
+							return function(WrappedV3, x, y, z)
+								v3_set(WrappedV3.V3, x, y, z)
+							end
+						end,
+		setX		=	function()
+							local v3_setX = v3.setX
+							return function(WrappedV3, x)
+								v3_setX(WrappedV3.V3, x)
+							end
+						end,
+		setY		=	function()
+							local v3_setY = v3.setY
+							return function(WrappedV3, y)
+								v3_setY(WrappedV3.V3, y)
+							end
+						end,
+		setZ		=	function()
+							local v3_setZ = v3.setZ
+							return function(WrappedV3, z)
+								return v3_setZ(WrappedV3.V3, z)
+							end
+						end,
+		reset		=	function()
+							local v3_reset = v3.reset
+							return function(WrappedV3)
+								v3_reset(WrappedV3.V3)
+							end
+						end,
+		add			=	function()
+							local v3_add = v3.add
+							return function(WrappedV3A, WrappedV3B)
+								v3_add(WrappedV3A.V3, WrappedV3B.V3)
+							end
+						end,
+		sub			=	function()
+							local v3_sub = v3.sub
+							return function(WrappedV3A, WrappedV3B)
+								v3_sub(WrappedV3A.V3, WrappedV3B.V3)
+							end
+						end,
+		mul			=	function()
+							local v3_mul = v3.mul
+							return function(WrappedV3A, number)
+								v3_mul(WrappedV3A.V3, number)
+							end
+						end,
+		div			=	function()
+							local v3_div = v3.div
+							return function(WrappedV3A, number)
+								v3_div(WrappedV3A.V3, number)
+							end
+						end,
+		eq			=	function()
+							local v3_eq = v3.eq
+							return function(WrappedV3A, WrappedV3B)
+								return v3_eq(WrappedV3A.V3, WrappedV3B.V3)
+							end
+						end,
+		magnitude	=	function()
+							local v3_magnitude = v3.magnitude
+							return function(WrappedV3)
+								return v3_magnitude(WrappedV3.V3)
+							end
+						end,
+		distance	=	function()
+							local v3_distance = v3.distance
+							return function(WrappedV3A, WrappedV3B)
+								return v3_distance(WrappedV3A.V3, WrappedV3B.V3)
+							end
+						end,
+		abs			=	function()
+							local v3_abs = v3.abs
+							return function(WrappedV3)
+								v3_abs(WrappedV3.V3)
+							end
+						end,
+		sum			=	function()
+							local v3_sum = v3.sum
+							return function(WrappedV3)
+								v3_sum(WrappedV3.V3)
+							end
+						end,
+		min			=	function()
+							local v3_min = v3.min
+							return function(WrappedV3)
+								return v3_min(WrappedV3.V3)
+							end
+						end,
+		max			=	function()
+							local v3_max = v3.max
+							return function(WrappedV3)
+								return v3_max(WrappedV3.V3)
+							end
+						end,
+		dot			=	function()
+							local v3_dot = v3.dot
+							return function(WrappedV3A, WrappedV3B)
+								return v3_dot(WrappedV3A.V3, WrappedV3B.V3)
+							end
+						end,
+		normalise	=	function()
+							local v3_normalise = v3.normalise
+							return function(WrappedV3)
+								v3_normalise(WrappedV3.V3)
+							end
+						end,
+		crossProduct=	function()
+							local v3_crossProduct = v3.crossProduct
+							return function(WrappedV3A, WrappedV3B)
+								return WrapNewInstance(v3_crossProduct(WrappedV3A.V3, WrappedV3B.V3))
+							end
+						end,
+		toRot		=	function()
+							local v3_toRot = v3.toRot
+							return function(WrappedV3)
+								return WrapNewInstance(v3_toRot(WrappedV3.V3))
+							end
+						end,
+		lookAt		=	function()
+							local v3_lookAt = v3.lookAt
+							return function(WrappedV3A, WrappedV3B)
+								return WrapNewInstance(v3_lookAt(WrappedV3A.V3, WrappedV3B.V3))
+							end
+						end,
+		toDir		=	function()
+							local v3_toDir = v3.toDir
+							return function(WrappedV3)
+								return WrapNewInstance(v3_toDir(WrappedV3.V3))
+							end
+						end,
+		toString		=	function()
+							local v3_toString = v3.toString
+							return function(WrappedV3)
+								return WrapNewInstance(v3_toString(WrappedV3.V3))
+							end
+						end,
+	}
 	
-	local WrappedV3 = setmetatable(V3NewFunctions[type(arg1)](arg1, args),V3WrapperMetaTable)
-	
-	table.insert(V3WrapperArchive, WrappedV3)
-	
-	return WrappedV3
+	local pairs = pairs
+	for Key, NewReplacementFunction in pairs(Functions) do
+		v3[Key] = NewReplacementFunction()
+	end
 end
 
---v3.new = V3New
-return V3New
+
+
+return v3
